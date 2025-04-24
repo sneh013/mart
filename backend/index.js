dc33f982d49ad84776ab2a16aa84519ec208a265
Shagun <162280742+ShagunHub@users.noreply.github.com>
2025-04-24 10:13:56 +0530

    started server first

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
-import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
- app.use("/api/users",userRoutes);
 
- 
+import connectDB from './config/db.js'
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+connectDB();
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.get('/', (req, res) => {
+    res.send('API is running...')
+});
 app.listen(port, () => 
 console.log(`server running on port:${port}`));
