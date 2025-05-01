23b90ecb787ac7aa240979f5405d3553c3231c34
sneh013 <sharmasneh013@gmail.com>
2025-05-01 10:47:52 +0530

    knk

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -5,7 +6,8 @@ import cookieParser from 'cookie-parser'
  import connectDB from './config/db.js'    
  import userRoute from './routes/userRoute.js'
  import categoryRoutes from './routes/categoryRoutes.js';
-
+ import productRoutes from './routes/productRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
  dotenv.config();
  const port = process.env.PORT || 5000;
@@ -16,6 +18,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
  app.use("/api/users",userRoute);
  app.use('/api/category',categoryRoutes);
+ app.use('/api/products', productRoutes);
+ app.use('/api/upload',uploadRoutes)
+
+ const __dirname=path.resolve();
+ app.use("/uploads",express.static(path.join(__dirname+ "/uploads")));
 
 app.listen(port, () => 
 console.log(`server running on port:${port}`));
